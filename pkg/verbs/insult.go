1004e205653c7295697e46a3efae08c54223a7ff
Adam Worrall <adam@worrall.cc>
2020-05-31 15:41:50 -0700

    Ensure new insults are used straight away

--- pkg/verbs/insult.go
+++ pkg/verbs/insult.go
@@ -8,6 +8,7 @@ import(
 
 type Insult struct{
 	Insults []string
+	New bool
 }
 
 func (i *Insult)Help() string { return "[user] [learn user is a limpet]" }
@@ -26,10 +27,17 @@ func (i *Insult)Process(vc VerbContext, args []string) string {
 	case "learn":
 		new := strings.Join(args[1:], " ")
 		i.Insults = append(i.Insults, new)
+		i.New = true
 		return "heh heh"
 		
 	default:
+		if len(i.Insults) == 0 { return "teach me" }
 		str := i.Insults[rand.Intn(len(i.Insults))]
+		if i.New {
+			// If we've just learned a new one, use it stratight away :)
+			str = i.Insults[len(i.Insults)-1]
+			i.New = false
+		}
 		target := strings.Join(args, " ")
 		str = regexp.MustCompile(`\buser\b`).ReplaceAllString(str, target)
 		return str
